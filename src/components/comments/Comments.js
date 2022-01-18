5060c06c191fd4d84ffc010de1f3b11a3d8a2f43
N00bG1rl <marttila.helen@gmail.com>
2022-01-18 12:43:32 +0200

    Write comments

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,13 +1,51 @@
-import { useState } from 'react'
+import { useState, useEffect, useCallback } from 'react'
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
 
-import styles from './Comments.module.css'
+import useHttp from '../../hooks/use-http'
 import NewCommentForm from './NewCommentForm'
+import { getAllComments } from '../../lib/api'
+import CommentsList from './CommentsList'
+import LoadingSpinner from '../UI/LoadingSpinner'
+import styles from './Comments.module.css'
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false)
+  const params = useParams()
+  const { quoteId } = params
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments)
+
+  useEffect(() => {
+    sendRequest(quoteId)
+  }, [quoteId, sendRequest])
 
   const handleCommentAdd = () => {
     setIsAddingComment(true)
+  }
+
+  // Wrap with useCallback for avoiding infinite loop
+  const handleAddedComment = useCallback(() => {
+    sendRequest(quoteId)
+  }, [sendRequest, quoteId])
+
+  let comments
+
+  if (status === 'pending') {
+    comments = (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    )
+  }
+
+  if (status === 'completed' && (loadedComments || loadedComments.length > 0)) {
+    comments = <CommentsList comments={loadedComments} />
+  }
+
+  if (
+    status === 'completed' &&
+    (!loadedComments || loadedComments.length === 0)
+  ) {
+    comments = <p className='centered'>No comments found.</p>
   }
 
   return (
@@ -18,8 +56,13 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && (
+        <NewCommentForm
+          quoteId={params.quoteId}
+          onAddedComment={handleAddedComment}
+        />
+      )}
+      {comments}
     </section>
   )
 }
